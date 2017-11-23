132309383aca8317ddb541810c153f96410ed856
root <root@Arch.localdomain>
2017-11-24 01:51:39 +0800

    update

--- 01_ProtectedMode/bootmain.c
+++ 01_ProtectedMode/bootmain.c
@@ -1,4 +1,4 @@
-void init_screen(int); 
+void init_screen(int);
 
 //called by bootasm.S
 void bootmain(void)
@@ -7,7 +7,7 @@ void bootmain(void)
 	init_screen(-1);
 
 	//Infinite loop
-	while(1)
+	while (1)
 	{
 		asm("hlt"); //inline assembly
 	}
@@ -16,25 +16,25 @@ void bootmain(void)
 //init the screen
 void init_screen(int color)
 {
-	char *ptr;//pointer
+	char *ptr; //pointer
 
 	//if color=-1.....
-	if(color==-1)
+	if (color == -1)
 	{
 		//vram(0xa0000~0xaffff).
 		//Reference: https://en.wikipedia.org/wiki/Video_Graphics_Array#Addressing_details
-		for(int i=0xa0000;i<0xaffff;i++) 
+		for (int i = 0xa0000; i < 0xaffff; i++)
 		{
-			ptr=(char *)i;
-			*ptr=i&0x0f;
+			ptr = (char *)i;
+			*ptr = i & 0x0f;
 		}
 	}
 	else
 	{
-		for(int i=0xa0000;i<0xaffff;i++)
+		for (int i = 0xa0000; i < 0xaffff; i++)
 		{
-			ptr=(char *)i;
-			*ptr=color;
+			ptr = (char *)i;
+			*ptr = color;
 		}
 	}
 }
